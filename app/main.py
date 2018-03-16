24b40da8c9d5eacdd4b9b6485025371aae2f0af6
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2018-03-16 16:15:14 +0100

    Move back to normal Flask. I prefer it to flask-restful.

--- app/main.py
+++ app/main.py
@@ -1,30 +1,51 @@
-from flask import Flask
-from flask_restful import Resource, Api
-from flask_restful import reqparse
-import os
-import pandas as pd
-import numpy as np
+from flask import Flask, jsonify, request, Response
 import json
-import html
 
 app = Flask(__name__)
-api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('js', type=str, help='Dummy JSON to store.')
+@app.errorhandler(404)
+def bad_input(error=None):
+    message = {
+            'status': 400,
+            'message': 'Bad Request, input was malformed.',
+    }
+    resp = jsonify(message)
+    resp.status_code = 400
 
-class DummyAPI(Resource):
-    def get(self):
-        try:
-            args = parser.parse_args()
-            data = args['js']
-            # Put on db.
+    return resp
 
-            return {"Message": data}, 200
-        except Exception as e:
-            raise e
-            return {"Message": "Bad get input!"}, 400
+@app.route('/add', methods=["GET", "POST"])
+def index():
+    """Help on route '/':
+
+    Sends a GET or POST for adding JSON value to the database.
+    
+    :param js: String containing the JSON string for GET.
+    :param json: JSON containing value for POST."""
+
+    data = None
+    if(request.method == "GET"):
+        data = request.args.get("js")
+        if(data):
+            # Check if valid JSON
+            try:
+                data = json.loads(data)
+                data = json.dumps(data)
+            except json.decoder.JSONDecodeError as e:
+                data = None
+
+    elif (request.method == "POST"):
+        data = request.get_json()
+        if (data):
+            data = json.dumps(data)
+        
+    if not data:
+        return bad_input()
+    else:
+        resp = jsonify(status=200, message=data)
+        resp.status_code = 200
+        
+        return resp
 
 if __name__ == '__main__':
-    api.add_resource(DummyAPI, '/add')
     app.run(host='127.0.0.1', debug=True, port=80)
