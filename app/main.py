cd8c6d97e8e4ec3c35533c629bd91ddfdefd8ee8
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2018-03-16 15:49:38 +0100

    Add some basic functionality, will refactor later.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,30 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_restful import reqparse
+import os
+import pandas as pd
+import numpy as np
+import json
+import html
 
+app = Flask(__name__)
+api = Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument('js', type=str, help='Dummy JSON to store.')
 
+class DummyAPI(Resource):
+    def get(self):
+        try:
+            args = parser.parse_args()
+            data = args['js']
+            # Put on db.
 
-def main():
-    pass
+            return {"Message": data}, 200
+        except Exception as e:
+            raise e
+            return {"Message": "Bad get input!"}, 400
 
 if __name__ == '__main__':
-    main()
+    api.add_resource(DummyAPI, '/add')
+    app.run(host='127.0.0.1', debug=True, port=80)
