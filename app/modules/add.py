119e2e86445f2c634200fdb85952e79565511743
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2018-03-17 18:17:22 +0100

    Change how parameters are passed for both add and retrieve. Add single retrieve with id endpoint.

--- app/modules/add.py
+++ app/modules/add.py
@@ -9,25 +9,22 @@ from .models import cache
 api = Namespace('add', description='Add new JSON objects to the store')
 
 add = api.model('add', {
-    'json': fields.String(required=True, description='The JSON to add'),
+    's': fields.String(required=True, description='The JSON string to add to the store.'),
 })
-@api.route('/')
-@api.param('json', 'The JSON to add')
-@api.doc(params={'json': 'The JSON to add'})
+
+@api.route('/<s>')
+@api.param('s', 'The JSON to add')
 class AddAPI(Resource):
     """Add new JSON object to the store"""
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('json', type=str, help='Dummy data to store, JSON format.')    
-        args = parser.parse_args(strict=True)
-
-        data = args['json']
+    
+    @api.doc("Add new JSON object to the store.")
+    def get(self, s):
+        data = s
 
         # Check if JSON is valid
         if(data):
             try:
                 data_loaded = json.loads(data)
-                
             except json.decoder.JSONDecodeError as e:
                 return utils.bad_request_400("Bad input.")
         
@@ -37,7 +34,6 @@ class AddAPI(Resource):
             # Invalidate cache.
             cache.clear()
             return utils.success_200(data)
-        
         else:
             return utils.bad_request_400("Missing input.")
         
--- app/modules/retrieve.py
+++ app/modules/retrieve.py
@@ -5,13 +5,32 @@ import utils
 from .models import JSONObject
 from .models import cache
 
-api = Namespace('retrieve', description='Retrieve all JSON objects in the store')
+api = Namespace('retrieve', description='Retrieve JSON objects from the store.')
+
+retrieve = api.model('retrieve', {
+    'id': fields.Integer(required=True, description='The document to retrieve.'),
+})
+
+@api.route('/<id>')
+@api.param('id', 'The JSON identifier.')
+class RetrieveAPI(Resource):
+    """Retrieve the JSON object matching the id from the store."""
+
+    @api.doc("Retrieve the JSON object matching the id from the store.")
+    @cache.memoize(50)
+    def get(self, id):
+        object = JSONObject.query.get(id)
+        if(object):
+            return utils.success_200((object.json_id, object.json_contents))
+        else:
+            return utils.bad_request_400("Could not find object with id: {}".format(id))
 
 @api.route('/')
-@api.doc()
-class RetrieveAPI(Resource):
+class RetrieveListAPI(Resource):
     """Retrieve all JSON objects in the store."""
-    @cache.cached(50, key_prefix='all objects')
+
+    @api.doc("Retrieve all JSON objects in the store.")
+    @cache.cached(50)
     def get(self):
         store = JSONObject.query.all()
         
